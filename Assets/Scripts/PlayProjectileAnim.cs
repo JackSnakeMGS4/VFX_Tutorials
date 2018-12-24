a9199f4783fbbbf9cf34cc346ce247aaf0b37789
Jaime Rivas <jaimerivas81@outlook.com>
2018-12-24 02:59:21 -0800

    first 15 minutes of tutorial

---
+++ Assets/Scripts/PlayProjectileAnim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayProjectileAnim : MonoBehaviour
+{
+    [SerializeField] GameObject mainProjectile;
+    [SerializeField] ParticleSystem mainPS;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(mainProjectile != null && mainPS != null)
+        {
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                mainProjectile.SetActive(true);
+            }
+
+            if (!mainPS.IsAlive())
+            {
+                mainProjectile.SetActive(false);
+            }
+        }
+    }
+}
